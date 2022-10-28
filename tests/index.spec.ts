69f638a4aeb4d84b4a91268f8c9756b9f1afce03
Jon Gull <25947147+JonrGull@users.noreply.github.com>
2022-10-28 17:41:49 +0900

    refactor: baseURL

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -2,12 +2,12 @@ import { expect, test } from "@playwright/test";
 
 test.describe("Index", () => {
   test("Input boxes should match", async ({ page }) => {
-    await page.goto("https://jg-mindset.vercel.app/login");
+    await page.goto("/login");
     await page.getByLabel("Email address*").fill("demo@test.com");
     await page.getByLabel("Password*").fill("123456");
     await page.getByRole("button", { name: "Sign in" }).click();
-    
-    await expect(page).toHaveURL("https://jg-mindset.vercel.app/");
+
+    await expect(page).toHaveURL("/");
 
     await page.locator("input").fill("Test test string");
     await expect(page.locator(".css-cktl6h").first()).toHaveText("Test test string");
