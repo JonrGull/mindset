d15a5a09af75fb2927ab16a631ab64ba1ab92bb7
Jon Gull <25947147+JonrGull@users.noreply.github.com>
2022-10-28 15:38:20 +0900

    add: index page test

---
+++ tests/index.spec.ts
@@ -0,0 +1,20 @@
+import { expect, test } from "@playwright/test";
+
+test.describe("Index", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("https://jg-mindset.vercel.app/login");
+    await page.getByLabel("Email address*").fill("demo@test.com");
+    await page.getByLabel("Password*").fill("123456");
+    await page.getByRole("button", { name: "Sign in" }).click();
+
+    await expect(page).toHaveURL("https://jg-mindset.vercel.app/");
+  });
+
+  test("Input boxes should match", async ({ page }) => {
+    await page.goto("https://jg-mindset.vercel.app/");
+    await expect(page).toHaveURL("https://jg-mindset.vercel.app/");
+    await page.locator("input").fill("Test test string");
+
+    await expect(page.locator(".css-cktl6h").first()).toHaveText("Test test string");
+  });
+});
