5d271a0e63e60e502d5673dea455ed27515fd9e6
Jon Gull <25947147+JonrGull@users.noreply.github.com>
2022-10-29 18:13:36 +0900

    feat: add blog tests

---
+++ tests/blogs.spec.ts
@@ -0,0 +1,33 @@
+import { expect, test } from "@playwright/test";
+
+test.beforeEach(async ({ page }) => {
+  await page.goto("/login");
+  await page.getByLabel("Email address*").fill("demo@test.com");
+  await page.getByLabel("Password*").fill("123456");
+  await page.getByRole("button", { name: "Sign in" }).click();
+});
+
+test.describe("Blogs page", () => {
+  test("write post should create new blog", async ({ page }) => {
+    await expect(page).toHaveURL("/");
+
+    await page.getByRole("button", { name: "Blogs" }).click();
+    await expect(page).toHaveURL("https://jg-mindset.vercel.app/blogs");
+    await page.getByRole("button", { name: "Write article" }).click();
+
+    expect(page.locator('section[role="dialog"]:has-text("A photo will be chosen for you.Post!Cancel")').isVisible());
+
+    await page.getByPlaceholder("Title").fill("Playwright Test");
+    await page.getByPlaceholder("Title").press("Tab");
+    await page.getByPlaceholder("Content").fill("This is a test from Playwright.");
+    await page.getByRole("button", { name: "Post!" }).click();
+
+    await page.getByText("Playwright TestThis is a test from Playwright.").isVisible();
+    await page
+      .locator('li:has-text("Playwright TestThis is a test from Playwright.Delete")')
+      .getByRole("button", { name: "Delete" })
+      .click();
+
+    await page.getByText("Playwright TestThis is a test from Playwright.").isHidden();
+  });
+});
